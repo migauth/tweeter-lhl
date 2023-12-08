176c20ef47e0e782fd9676a70d092bcb5181d6c3
Michael Gauthier <mikegauthier222@hotmail.com>
2023-12-08 13:54:47 -0800

    fix error-counter not changing

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -5,15 +5,12 @@ let count = 140;
 $(document).ready(function () {
   $("#tweet-text").on('input', function () {
     let counter = count - ($("#tweet-text").val().length) // Count minus how many characters
-
     $(this).parents()[0][2].textContent = counter;
-
     if (counter < 0) { // Counter turns red if negative
       $(this).parents()[0][2].className = "negative";
     } else {
       $(this).parents()[0][2].className = "counter";
     }
-
   });
 });
 
