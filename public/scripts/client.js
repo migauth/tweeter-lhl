0bd902f8d67ed2fee338b5e23cb9ed533eead755
Michael Gauthier <mikegauthier222@hotmail.com>
2023-12-12 11:17:53 -0800

    fetch tweets with ajax-clear tweet container for new tweets-add timeago

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -5,31 +5,31 @@
 //  */
 
 // Template for tweeter user data
-const data = [
-  {
-    "user": {
-      "name": "Newton",
-      "avatars": "https://i.imgur.com/73hZDYK.png"
-      ,
-      "handle": "@SirIsaac"
-    },
-    "content": {
-      "text": "If I have seen further it is by standing on the shoulders of giants"
-    },
-    "created_at": 1461116232227
-  },
-  {
-    "user": {
-      "name": "Descartes",
-      "avatars": "https://i.imgur.com/nlhLi3I.png",
-      "handle": "@rd"
-    },
-    "content": {
-      "text": "Je pense , donc je suis"
-    },
-    "created_at": 1461113959088
-  }
-]
+// const data = [
+//   {
+//     "user": {
+//       "name": "Newton",
+//       "avatars": "https://i.imgur.com/73hZDYK.png"
+//       ,
+//       "handle": "@SirIsaac"
+//     },
+//     "content": {
+//       "text": "If I have seen further it is by standing on the shoulders of giants"
+//     },
+//     "created_at": 1461116232227
+//   },
+//   {
+//     "user": {
+//       "name": "Descartes",
+//       "avatars": "https://i.imgur.com/nlhLi3I.png",
+//       "handle": "@rd"
+//     },
+//     "content": {
+//       "text": "Je pense , donc je suis"
+//     },
+//     "created_at": 1461113959088
+//   }
+// ]
 
 // Create HTML template for tweets
 const createTweetElement = function (tweet) {
@@ -45,7 +45,7 @@ const createTweetElement = function (tweet) {
       <p>${tweet.content.text}</p>
       <hr>
       <footer class="post footer">
-        <span>${tweet.created_at}</span>
+        <span>${timeago.format(tweet.created_at)}</span>
         <div>
           <i class="fa-solid fa-flag"></i>
           <i class="fa-solid fa-retweet"></i>
@@ -60,13 +60,38 @@ const createTweetElement = function (tweet) {
 const renderTweets = function (tweets) {
   // loops through tweets
   for (const i of tweets) {
-     // calls createTweetElement for each tweet
     let $tweet = createTweetElement(i)
-    // takes return value and appends it to the tweets container
-    $('#tweets-container').append($tweet);
+    $('#tweets-container').prepend($tweet);
   }
 };
 
-$(document).ready(function() {
-  renderTweets(data)
+$(document).ready(function () {
+
+  const loadTweets = function () {
+    $.ajax('http://localhost:8080/tweets', { method: 'GET' })
+      .then(function (tweets) {
+        renderTweets(tweets);
+      });
+  }
+
+  loadTweets()
+
+  $("#new-tweet").on("submit", function (event) { // Event handler
+    const $tweetContainer = $('#tweets-container');
+    event.preventDefault(); // Prevents form submission
+    let serial = $(this).serialize();
+    $.ajax({
+      type: "POST",
+      url: "http://localhost:8080/tweets",
+      data: serial,
+      success: () => {
+        $tweetContainer.empty()
+        loadTweets()
+      },
+      dataType: "text"
+    });
+  });
+
 })
+
+
