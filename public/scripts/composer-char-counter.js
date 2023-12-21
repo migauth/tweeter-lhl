f078d6f80dec18f88aab8d43879f035b1231013a
Michael Gauthier <mikegauthier222@hotmail.com>
2023-12-21 09:19:40 -0800

    fix variable declarations

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,10 +1,8 @@
 // jQuery counter for tweeter
 
-let count = 140;
-
 $(document).ready(function() {
   $("#tweet-text").on('input', function() {
-    let counter = count - ($("#tweet-text").val().length); // Count minus how many characters
+    const counter = 140 - ($("#tweet-text").val().length); // Count minus how many characters
     $(this).parents()[0][2].textContent = counter;
     if (counter < 0) { // Counter turns red if negative
       $(this).parents()[0][2].className = "negative";
